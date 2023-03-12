b17d29ab1920f85531d279e873734c43c7780620
HeliZakay <heli.z@next-insurance.com>
2023-03-12 02:39:52 +0200

    add import

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const fs = require("fs");
+const path = require("path");
 
 const POKEMONS_FILE = "pokemon-data/pokedex.json";
 
